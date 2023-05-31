4ebfc2ef9b1bb77e27d91a8d6f52649811567331
thesagni <129326306+thesagni@users.noreply.github.com>
2023-05-31 13:44:25 +0200

    Create tests.sh

---
+++ project/tests.sh
@@ -0,0 +1,15 @@
+# Execute the data pipeline
+python3 AutoPipeLine.py
+
+ 
+
+data_files="Bicycle_data.sqlite"
+
+ 
+
+# Check if the output file(s) exist
+if [ -f "$data_files" ]; then
+    echo "Yes!! Output file $data_files is available."
+else
+    echo "No! Output file $data_files is not available."
+fi
