25568d09fcdc457ea960e8a430865c56f251a2fc
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-21 12:46:45 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,13 +1,18 @@
 import pandas as pd
-from sqlalchemy import String, TEXT, INTEGER, Float, DECIMAL,BIGINT
+import sqlite3
+from io import BytesIO
+import requests
 
-cols_sql = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 # Download the CSV file
 url = "https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
-df = pd.read_csv(url, encoding="latin1", skiprows=6, skipfooter=4, usecols=cols_sql)
+response = requests.get(url)
+data = response.content
+
+# Read CSV data into a DataFrame
+df = pd.read_csv(BytesIO(data), encoding='iso-8859-1', skiprows=6, skipfooter=4)
 
 # Rename columns
-cols_map = {
+columns_mapping = {
     'A': 'date',
     'B': 'CIN',
     'C': 'name',
@@ -19,15 +24,15 @@ cols_map = {
     'BK': 'plugInHybrid',
     'BU': 'others'
 }
-df = df.rename(columns=cols_map)
+df = df.rename(columns=columns_mapping)
 
 # Keep only required columns
-required_cols = list(cols_map.values())
-df = df[required_cols]
+required_columns = list(columns_mapping.values())
+df = df[required_columns]
 
 # Validate and filter data
 df = df[df['CIN'].str.match(r'^0?\d{5}$')]
-df = df[df[required_cols[3:]].apply(lambda x: x.str.isnumeric() & (x.astype(int) > 0)).all(axis=1)]
+df = df[df[required_columns[3:]].apply(lambda x: x.str.isnumeric() & (x.astype(int) > 0)).all(axis=1)]
 
 # Convert data types
 data_types = {
@@ -45,4 +50,29 @@ data_types = {
 df = df.astype(data_types)
 
 # Create SQLite database and write data to the "cars" table
-df.to_sql('cars','sqlite:///cars.sqlite', if_exists='replace', index=False)
+conn = sqlite3.connect('cars.sqlite')
+cursor = conn.cursor()
+
+# Create "cars" table
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS cars (
+    date TEXT,
+    CIN TEXT,
+    name TEXT,
+    petrol INTEGER,
+    diesel INTEGER,
+    gas INTEGER,
+    electro INTEGER,
+    hybrid INTEGER,
+    plugInHybrid INTEGER,
+    others INTEGER
+);
+'''
+cursor.execute(create_table_query)
+
+# Insert data into "cars" table
+df.to_sql('cars', conn, if_exists='replace', index=False)
+
+# Commit changes and close the database connection
+conn.commit()
+conn.close()
