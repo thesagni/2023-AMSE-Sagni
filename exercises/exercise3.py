417232bf36f07e5694fe2e563f8bdf8e28aa2c8c
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-21 13:04:50 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -5,7 +5,7 @@ url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00
 cols_sql = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro','hybrid', 'plugInHybrid', 'others']
 
-df = pd.read_csv(url, sep=';', encoding="latin1", header=None, skiprows=6, skipfooter=4,
+df = pd.read_csv(url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
                  usecols=cols_sql, names=columns, engine='python',
                  converters={'CIN': str})
 
