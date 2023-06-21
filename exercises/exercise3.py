5110f5345855bf74cea98c65a78e8b69d7d4c622
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-21 14:29:34 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,16 +1,23 @@
-
 import pandas as pd 
 
 url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
 cols = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
-columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro','hybrid', 'plugInHybrid', 'others']
+columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
 df = pd.read_csv(url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
                  usecols=cols, names=columns, engine='python',
                  converters={'CIN': str})
 
-# Convert data types
-data_types = {'petrol': 'int64', 'diesel': 'int64', 'gas': 'int64', 'electro': 'int64', 'hybrid': 'int64', 'plugInHybrid': 'int64', 'others': 'int64'}
-df = df.astype(data_types).astype({"CIN": str}, copy=False, errors='ignore')
+# Replace '-' values with NaN
+df = df.replace('-', float('nan'))
 
+# Convert data types
+data_types = {'petrol': 'float64', 'diesel': 'float64', 'gas': 'float64', 'electro': 'float64',
+              'hybrid': 'float64', 'plugInHybrid': 'float64', 'others': 'float64'}
+df = df.astype(data_types, copy=False)
+
+# Convert CIN column to string type
+df['CIN'] = df['CIN'].astype(str)
+
+# Write data to SQLite database
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
