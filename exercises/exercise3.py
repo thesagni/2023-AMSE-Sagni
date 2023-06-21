a7b87081c65bf870bf6f69fa10b263e3a2aec81e
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-21 14:42:11 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -6,17 +6,17 @@ cols = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
 df = pd.read_csv(url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
-                 usecols=cols, names=columns, engine='python',
-                 converters={'CIN': str})
+                 usecols=cols, names=columns, engine='python')
 
 
 df = df[df['petrol'].apply(lambda x: not re.search(r'-', str(x)))]
 
 
 # Convert data types
-data_types = {'petrol': 'float64', 'diesel': 'float64', 'gas': 'float64', 'electro': 'float64',
-              'hybrid': 'float64', 'plugInHybrid': 'float64', 'others': 'float64'}
-df = df.astype(data_types, copy=False)
+data_types = {'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'}
+df = df.astype(data_types)
+# Convert CIN column to string type
+df['CIN'] = df['CIN'].astype(str)
 
 
 
