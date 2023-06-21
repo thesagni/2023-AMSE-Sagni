43919b0a8b22b5724af7258c580c2afb2870301e
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-21 14:27:22 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -10,8 +10,7 @@ df = pd.read_csv(url, sep=';', encoding="latin1", header=None, skiprows=7, skipf
                  converters={'CIN': str})
 
 # Convert data types
-#df = df[df["petrol"].str.contains("-")==False]
-data_types = {'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'}
+data_types = {'petrol': 'int64', 'diesel': 'int64', 'gas': 'int64', 'electro': 'int64', 'hybrid': 'int64', 'plugInHybrid': 'int64', 'others': 'int64'}
 df = df.astype(data_types).astype({"CIN": str}, copy=False, errors='ignore')
 
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
