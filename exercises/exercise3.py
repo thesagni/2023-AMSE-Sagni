12159d7331a58c360f05e737df0b7f6134a052f1
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-21 14:49:05 +0200

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,25 +1,23 @@
 import pandas as pd 
-import re
 
 url = 'https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv'
 cols = [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]
 columns = ['date', 'CIN', 'name', 'petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-df = pd.read_csv(url, sep=';', encoding="latin1", header=None, skiprows=7, skipfooter=4,
-                 usecols=cols, names=columns, engine='python')
+df = pd.read_csv(url, sep=';', encoding="iso-8859-1", header=None, skiprows=6, skipfooter=4,
+                 usecols=cols, names=columns, engine='python', na_values='.', thousands=',',
+                 dtype={'CIN': str})
 
+# Filter out rows with '-' in petrol column
+df = df[~df['petrol'].str.contains('-', na=False)]
 
-df = df[df['petrol'].apply(lambda x: not re.search(r'-', str(x)))]
-df = df[df['CIN'].apply(lambda x: re.match(r'^\d{5}$', str(x)) is not None)]
-
+# Ensure 'CIN' column has exactly five characters
+df = df[df['CIN'].str.len() == 5]
 
 # Convert data types
-data_types = {'petrol':'int64', 'diesel':'int64', 'gas':'int64', 'electro':'int64', 'hybrid':'int64', 'plugInHybrid':'int64', 'others':'int64'}
+data_types = {'petrol': 'int64', 'diesel': 'int64', 'gas': 'int64', 'electro': 'int64',
+              'hybrid': 'int64', 'plugInHybrid': 'int64', 'others': 'int64'}
 df = df.astype(data_types)
-# Convert CIN column to string type
-df['CIN'] = df['CIN'].astype(str)
-
-
 
 # Write data to SQLite database
 df.to_sql('cars', 'sqlite:///cars.sqlite', if_exists='replace', index=False)
