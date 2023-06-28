e33443cf8e2f14bbc5e528c75ffa9928c19708b2
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-28 13:06:11 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,14 +1,15 @@
 # Execute the data pipeline
 python3 AutoPipeLine.py
 
-data_files="Bicycle_data.sqlite"
+data_file="data/Bicycle_data.sqlite"
 
-# Check if the output file(s) exist
-if [ -f "$data_files" ]; then
-    echo "Yes! Output file $data_files is available."
+# Check if the output file exists
+if [ -f "$data_file" ]; then
+    echo "Yes! Output file $data_file is available."
 else
-    echo "No!! Output file $data_files is not available."
+    echo "No! Output file $data_file is not available."
 fi
+
 
 
 
