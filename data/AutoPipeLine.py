df81319ebe53a1c0159e64ea8560a4fb2584a78c
thesagni <129326306+thesagni@users.noreply.github.com>
2023-06-28 12:59:48 +0200

    Update AutoPipeLine.py

--- data/AutoPipeLine.py
+++ data/AutoPipeLine.py
@@ -1,41 +1,75 @@
+import os
 import pandas as pd
-#downloading 1st data souce
+import sqlite3
 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2020-07.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2020-08.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2020-09.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2020-10.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2020-11.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2020-12.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2021-01.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2021-02.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2021-03.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2021-04.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2021-05.csv',sep=',') 
-df = pd.read_csv('https://github.com/od-ms/radverkehr-zaehlstellen/blob/main/100031297/2021-06.csv',sep=',') 
+# Create the /data folder in the current directory if it doesn't exist
+data_folder = 'data'
+os.makedirs(data_folder, exist_ok=True)
 
+# Define the paths to the CSV files
+csv_files = [
+    'C:/Users/reham/Desktop/2020-06.csv',
+    'C:/Users/reham/Desktop/2020-07.csv',
+    'C:/Users/reham/Desktop/2020-08.csv',
+    'C:/Users/reham/Desktop/2020-09.csv',
+    'C:/Users/reham/Desktop/2020-10.csv',
+    'C:/Users/reham/Desktop/2020-11.csv',
+    'C:/Users/reham/Desktop/2020-12.csv',
+    'C:/Users/reham/Desktop/2021-01.csv',
+    'C:/Users/reham/Desktop/2021-02.csv',
+    'C:/Users/reham/Desktop/2021-03.csv',
+    'C:/Users/reham/Desktop/2021-04.csv',
+    'C:/Users/reham/Desktop/2021-05.csv',
+    'C:/Users/reham/Desktop/2021-06.csv',
+    'C:/Users/reham/Desktop/2021-07.csv',
+    'C:/Users/reham/Desktop/2021-08.csv'
+]
 
-df.columns.value[0]= 'Total Bicylce count'
+# Read and concatenate the CSV files
+dfs = []
+for file in csv_files:
+    df = pd.read_csv(file, encoding='latin-1')
+    dfs.append(df)
 
+# Concatenate all DataFrames into a single DataFrame
+combined_df = pd.concat(dfs)
 
-#downloading 2nd data souce
-#data source- https://www-genesis.destatis.de/genesis/online?language=en&sequenz=statistikTabellen&selectionname=46241#abreadcrumb
+# Rename the desired column
+combined_df.rename(columns={'Unnamed: 1': 'Total Bicycle count'}, inplace=True)
 
-df = pd.read_csv('C:/Users/reham/Desktop/subj sem 4/AMSE(DATA ENGG)/data/data source 2/AccidentData.csv',sep=',') 
+# Save the combined DataFrame to a CSV file in the /data directory
+combined_df.to_csv(os.path.join(data_folder, 'combined_data.csv'), index=False)
 
-#renaming needed rows :
-df.rows.value[24] = 'Total Accidents - Inside built-up areas'
-df.rows.value[25] = 'Total Accidents - Outside built-up areas'
-df.rows.value[26] = 'Total Accidents - On motorways/freeways'
-df.rows.value[27] = 'Total Accidents'
+# Read the AccidentData2.excel file
+accident_df =pd.read_excel('C:/Users/reham/Desktop/AccidentData2.xlsx')
 
-#downloading 3rd data souce
-#data source- https://muenster.polizei.nrw/artikel/verkehrsunfallstatistik-2021-1
+# Rename the desired rows
 
-df = pd.read_excel('C:/Users/reham/Desktop/subj sem 4/AMSE(DATA ENGG)/data/datasource3/Accident Data Münster.xlsx') 
+accident_df.iloc[21, 0] = 'Total Accidents'
 
-#renaming needed rows :
-df.rows.value[4] = 'Total Accidents - Münster'
+# Save the accident_df to a CSV file in the /data directory
+accident_df.to_csv(os.path.join(data_folder, 'accident_data.csv'), index=False)
 
-#saving table to sqlite file
-df.to_sql('Bicycle_Accidents','sqlite:///Bicycle_data.sqlite', if_exists='replace', index=False)
+# Read the Accident Data Münster.xlsx file
+munster_df = pd.read_excel('C:/Users/reham/Desktop/Accident Data Münster2.xlsx')
+
+# Rename the desired row
+munster_df.iloc[2, 0] = 'Total Accidents - Münster'
+
+# Save the munster_df to a CSV file in the /data directory
+munster_df.to_csv(os.path.join(data_folder, 'munster_data.csv'), index=False)
+
+# Create a SQLite connection
+conn = sqlite3.connect(os.path.join(data_folder, 'Bicycle_data.sqlite'))
+
+# Save the combined_df to the SQLite database
+combined_df.to_sql('Bicycle_Count', conn, if_exists='replace', index=False)
+
+# Save the accident_df to the SQLite database
+accident_df.to_sql('Accident_Data', conn, if_exists='replace', index=False)
+
+# Save the munster_df to the SQLite database
+munster_df.to_sql('Munster_Accidents', conn, if_exists='replace', index=False)
+
+# Close the database connection
+conn.close()
